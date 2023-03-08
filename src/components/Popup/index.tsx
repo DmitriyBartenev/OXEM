73ae62b937b8d1568676f02eed8cd92435fee044
DmitriyBartenev <bartenevdmitriy@bk.ru>
2023-03-08 13:11:07 +0400

    fixed db.json

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -29,7 +29,7 @@ interface PopupProps {
     setOpenPopup: Dispatch<SetStateAction<boolean>>
 }
 
-const phoneRegExp = /^\+7 \(\d{3}\) \d{3} \d{2} \d{2}$/;
+const phoneRegExp = /^\+7 \d{3} \d{3} \d{2} \d{2}$/;
 const nameRegExp = /^[А-Яа-я\s]+$/;
 
 const schema = yup.object().shape({
