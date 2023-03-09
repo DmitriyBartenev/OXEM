638154897c7f2badc722b3ee65bd35db465e1b7d
DmitriyBartenev <bartenevdmitriy@bk.ru>
2023-03-09 15:19:38 +0400

    fixed validation

--- src/components/Popup/validation.ts
+++ src/components/Popup/validation.ts
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 
 const phoneRegExp = /^\+7 \d{3} \d{3} \d{2} \d{2}$/;
-const nameRegExp = /^[А-Яа-я\s]+$/;
+const nameRegExp = /^[А-Яа-яA-Za-z\s]+$/;
 
 export const schema = yup.object().shape({
     phoneNumber: yup.string().required('Phone field is a required').matches(phoneRegExp, 'Phone number is not valid'),
